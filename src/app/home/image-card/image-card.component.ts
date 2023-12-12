01f5ec94a7e2b2f281fb292c74b29b022fe310ca
olakouns <kounassolazare@gmail.com>
2023-12-12 22:06:55 +0000

    update

--- src/app/home/image-card/image-card.component.ts
+++ src/app/home/image-card/image-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {AfterContentInit, Component, NgZone, OnDestroy, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-image-card',
@@ -7,6 +7,29 @@ import { Component } from '@angular/core';
   templateUrl: './image-card.component.html',
   styleUrl: './image-card.component.scss'
 })
-export class ImageCardComponent {
+export class ImageCardComponent implements OnInit, OnDestroy{
+  slides = ["slide-1.png", "slide-2.png", "slide-3.png"];
+  currentSlide = this.slides[0];
+
+  id : any;
+  constructor(private ngZone: NgZone) {}
+  ngOnInit(): void {
+    this.ngZone.runOutsideAngular(() => setInterval(() => {
+      const currentIndex = this.slides.indexOf(this.currentSlide);
+      this.ngZone.run(() => {
+        if (currentIndex < this.slides.length - 1) {
+          this.currentSlide = this.slides[currentIndex + 1];
+        } else {
+          this.currentSlide = this.slides[0];
+        }
+      });
+    }, 5000));
+  }
+
+  ngOnDestroy(): void {
+    if (this.id) {
+      clearInterval(this.id);
+    }
+  }
 
 }
